e53d265c41bdf3e2ac6e7839eebf28869b86b53e
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-05-19 10:55:45 +0100

    add delete operation to complete CRUD

--- crud.py
+++ crud.py
@@ -71,3 +71,11 @@ def update_book(book_id: int, updated_book: BookUpdate):
             book['publish_date'] = updated_book.publish_date
             return book
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+@app.delete("/book/{book_id}")
+def delete_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return {"message": "Book deleted successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
