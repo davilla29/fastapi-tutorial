b2ec67ec18523a1e0c3feec3c6036aa0e7d26a5a
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-05-20 08:22:29 +0100

    add code to generate secret key

--- auth/main.py
+++ auth/main.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 import models, schemas, utils
 from auth_database import get_db, Base, engine
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
